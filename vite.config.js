517ae850c55eb3dce3db8f5e838ff926edba3e13
Jawad656567 <ja289327@gmail.com>
2025-09-22 10:04:13 +0500

    new config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist", // Vercel ke liye correct output dir
+  },
+  server: {
+    port: 3000,
+  },
+});
